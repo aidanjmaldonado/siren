ada8316943d46f4df3043981dddccc5605853dff
Aidan Maldonado <aidanmaldonado@eduroam-169-233-139-6.ucsc.edu>
2025-04-03 17:53:49 -0700

    Add decompose.py

--- decompose.py
+++ decompose.py
@@ -8,5 +8,53 @@ class Decomposer():
         pass
 
     def decompose(self, amplitudes, source) -> np.array:
-        isolated_amplitudes = amplitudes
+        if source == "bass":
+            isolated_amplitudes = self.isolate_bass(amplitudes)
+        if source == "guitar":
+            isolated_amplitudes = self.isolate_bass(amplitudes)
+        if source == "vocals":
+            isolated_amplitudes = self.isolate_bass(amplitudes)
+        if source == "drums":
+            isolated_amplitudes = self.isolate_bass(amplitudes)
+        if source == "synth":
+            isolated_amplitudes = self.isolate_bass(amplitudes)
+        if source == "brass":
+            isolated_amplitudes = self.isolate_bass(amplitudes)
+
         return isolated_amplitudes
+
+    def isolate_bass(self, amplitudes) -> np.array:
+        """
+        To-do
+        """
+        return amplitudes
+
+    def isolate_guitar(self, amplitudes) -> np.array:
+        """
+        To-do
+        """
+        return amplitudes
+
+    def isolate_vocals(self, amplitudes) -> np.array:
+        """
+        To-do
+        """
+        return amplitudes
+
+    def isolate_drums(self, amplitudes) -> np.array:
+        """
+        To-do
+        """
+        return amplitudes
+
+    def isolate_synth(self, amplitudes) -> np.array:
+        """
+        To-do
+        """
+        return amplitudes
+
+    def isolate_brass(self, amplitudes) -> np.array:
+        """
+        To-do
+        """
+        return amplitudes
